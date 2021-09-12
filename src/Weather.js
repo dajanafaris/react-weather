b843e30934cc2c5df5c9e545b2c8e3d5b8c56867
Dajana <djnfrs255@gmail.com>
2021-09-12 13:00:28 +0200

    adding responsive

--- src/Weather.js
+++ src/Weather.js
@@ -1,82 +1,76 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 import "./Weather.css";
 import WeatherForecast from "./WeatherForecast";
 
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-export default function Weather(props){
-  const [weatherData, setWeatherData] = useState({ready: false});
-  const[city, setCity] = useState (props.defaultCity);
+  function handleResponse(response) {
+    console.log(response.data);
 
-function handleResponse(response){
-  console.log(response.data);
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      date: new Date(response.data.dt * 1000),
+      icon: response.data.weather[0].icon,
+    });
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-  setWeatherData({
-    ready:true,
-    coordinates: response.data.coord,
-    temperature: response.data.main.temp,
-    wind:response.data.wind.speed,
-    city: response.data.name,
-    humidity:response.data.main.humidity,
-    description:response.data.weather[0].description,
-    date: new Date (response.data.dt * 1000),
-    icon: response.data.weather[0].icon,
-  });
-}
-function handleSubmit(event){
-  event.preventDefault();
-  search();
-}
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
-function handleCityChange(event) {
-  setCity (event.target.value);
-}
+  function search() {
+    const apiKey = "1e3a1ed81fb1d1edbb19cf958878c70c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-
-
-function search() {
-const apiKey ="1e3a1ed81fb1d1edbb19cf958878c70c"
-  let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-}
-
-if (weatherData.ready) {
-
-    return(
-        <div className="App">
-      <div className="weather-app">
-        <div className="weather-wrapper">
-          <form className="mb-3" onSubmit={handleSubmit} >
-            <div className="row">
-              <div className="col-6">
-                <input
-                  type="search"
-                  className="form-control"
-                  id="city-input"
-                  placeholder="Search a city ..."
-                  onChange={handleCityChange}
-                />
+  if (weatherData.ready) {
+    return (
+      <div className="App">
+        <div className="weather-app">
+          <div className="weather-wrapper">
+            <form className="mb-3" onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-6">
+                  <input
+                    type="search"
+                    className="form-control"
+                    id="city-input"
+                    placeholder="Search a city ..."
+                    onChange={handleCityChange}
+                  />
+                </div>
+                <div className="col-3">
+                  <input
+                    type="submit"
+                    value="Search"
+                    className="btn btn-primary w-100"
+                  />
+                </div>
               </div>
-              <div className="col-3">
-                 <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-              </div>
-        
-            </div>
-          </form>
-          <WeatherInfo data={weatherData}/>
-          <WeatherForecast coordinates={weatherData.coordinates}/>
+            </form>
+            <WeatherInfo data={weatherData} />
+            <WeatherForecast coordinates={weatherData.coordinates} />
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-  );
-} 
-else{
-  search();
-  return "Loading";
-}
+    );
+  } else {
+    search();
+    return "Loading";
+  }
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,53 +3,49 @@ import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import Logo from "./Logo.svg";
 
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <h1 id="city">{props.data.city}</h1>
 
-export default function WeatherInfo(props){
-    return (
-        <div className="WeatherInfo">
-        
-        <h1 id="city">{props.data.city}</h1>
+      <h2 className="date">
+        <FormattedDate date={props.data.date} />
+      </h2>
 
+      <h3 className="description">
+        <span id="date"> </span>
+        <span className="description"> {props.data.description}</span>
+      </h3>
+      <div className="float-left">
+        <WeatherIcon code={props.data.icon} size={60} />
+        <span className="temperature">
+          {Math.round(props.data.temperature)}
+        </span>
+        <span className="units"> C° </span>
 
-          <h2 className= "date">
-               <FormattedDate date={props.data.date}/>
-                </h2>
-      
-                 <h3 className="description">
-             <span id="date"> </span>
-              <span className="description"> {props.data.description}</span> 
-          </h3>
-            <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={60} />
-               <span className="temperature">{Math.round(props.data.temperature)}</span>
-               <span className="units"> C° </span>
-            </div> 
-            
-          <div className="row">
-            <div className="col-6">
-              <div className="center-block">
-             
-<div>
-                <img src = {Logo} width="300" alt="logo" className="Visual"/>
-</div>
-                </div>
-           </div>
-                
-            <div className="col-2">
-              <ul className="weather-info"></ul>
-            
-              <li>
-                <span> Humidity:{props.data.humidity}%</span>
-              </li>
-              <li>
-               <span> Wind:{props.data.wind}km/h</span>
-              </li>
+        <ul className="weather-info">
+          <li>
+            <span> Humidity:{props.data.humidity}%</span>
+          </li>
+          <li>
+            <span> Wind:{props.data.wind}km/h</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <div className="center-block">
+            <div>
+              <img src={Logo} width="300" alt="logo" className="Visual" />
             </div>
-            <src id="icon" />
-          </div>
-          <div className="weather-forecast" id="forecast">     
           </div>
         </div>
-      
-    );
+
+        <div className="col-2"></div>
+        <src id="icon" />
+      </div>
+      <div className="weather-forecast" id="forecast"></div>
+    </div>
+  );
 }
